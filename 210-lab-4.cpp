0e16a670f94b56ef72f088284a379d94d8b35938
Edwin Burwell <edwinburwell758@gmail.com>
2026-08-30 13:13:39 -0700

    Assignment milestone (v1)

--- 210-lab-4.cpp
+++ 210-lab-4.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
 
+// The entry point of the program.
+//
+// Sequentially prints integer numbers in the interval [1, 100] in ascending order to `stdout`.
+// 
+// Note: A trailing space follows each number, including the final number.
+//
+// ### Arguments
+//
+// - None
+//
+// ### Returns
+//
+// A signed integer exit code, where `0` means success.
 int main() {
-    // complete this code in order to make it output
-    // the numbers 1-100
-
-    for ()
+    for (int i = 1; i <= 100; i++)
         cout << i << " ";
 
     return 0;
