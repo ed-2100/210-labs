076e4175bbe28b3f728bddab433b8929f0330048
Edwin Burwell <edwinburwell758@gmail.com>
2026-08-31 13:28:13 -0700

    Automatic commit 083126132813

--- check_for_update.py
+++ check_for_update.py
@@ -1,16 +1,20 @@
 import subprocess
 import datetime
+import time
 
 while True:
-
-
     if subprocess.check_output(["git", "status"]):
-        message = f"Periodic commit {datetime.datetime.now()}"
+        t = datetime.datetime.now()
+
+        print("Making commit... ", end="")
+
+        message = f"Automatic commit {t.strftime("%m%d%y%H%M%S")}"
         
-        # subprocess.run(["git", "add", "."])
-        # subprocess.run(["git", "commit", "-m", ])
-        print()
+        subprocess.run(["git", "add", "."])
+        subprocess.run(["git", "commit", "-m", message])
+
+        print(f"\"{message}\"")
     else:
-        pass
+        print("Skipping commit: no changes")
 
-
+    time.sleep(10 * 60)
