1ce2e465f302afb354f0c44bc8f0fec01a21b9c4
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-05 09:27:57 -0700

    Automatic commit 090526092757

--- lab-4/main.cpp
+++ lab-4/main.cpp
@@ -11,22 +11,14 @@ struct Color {
     uint8_t blue;
 };
 
+// Prints a table of `Color`s to `f`.
+//
+// Arguments:
+//
+// - `f`: The `ostream` to print to
+// - `colors`: The array of color values
+// - `n_colors`: The length of `colors`
 void print_colors(ostream& f, const Color *colors, size_t n_colors);
-
-void print_colors(ostream& f, const Color *colors, size_t n_colors) {
-    f << "| Color # | R value | G value | B value |\n"
-      << "| ------- | ------- | ------- | ------- |\n";
-    
-    for (size_t i = 0; i < n_colors; i++) {
-        const Color& color = colors[i];
-
-        f <<    "|   " << setw(3) << i + 1
-          << "   |   " << setw(3) << static_cast<int>(color.red)
-          << "   |   " << setw(3) << static_cast<int>(color.green)
-          << "   |   " << setw(3) << static_cast<int>(color.blue)
-          << "   |\n";
-    }
-}
 
 int main() {
     Color test_color = {
@@ -40,5 +32,17 @@ int main() {
     return 0;
 }
 
+void print_colors(ostream& f, const Color *colors, size_t n_colors) {
+    f << "Color #   R value   G value   B value\n"
+      << "-------   -------   -------   -------\n";
+    
+    for (size_t i = 0; i < n_colors; i++) {
+        const Color& color = colors[i];
 
-
+        f <<      "  " << setw(3) << i + 1
+          << "       " << setw(3) << static_cast<int>(color.red)
+          << "       " << setw(3) << static_cast<int>(color.green)
+          << "       " << setw(3) << static_cast<int>(color.blue)
+          << "\n";
+    }
+}
