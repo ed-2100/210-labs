37a6e5791f529ab639dff2e5ac62ef46d11bb200
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-05 09:07:57 -0700

    Automatic commit 090526090757

--- lab-4/main.cpp
+++ lab-4/main.cpp
@@ -11,20 +11,21 @@ struct Color {
     uint8_t blue;
 };
 
-void print_color_table(ostream f, const const Color colors[]);
+void print_color_table(ostream f, const Color *colors, size_t n_colors);
 void print_color_row(ostream f, const Color &color);
 
-void print_color_table(ostream f, const const Color colors[]) {
+void print_color_table(ostream f, const Color *colors, size_t n_colors) {
     f << "| Color # | R value | G value | B value |\n"
       << "| ------- | ------- | ------- | ------- |\n";
     
-    for (const auto &color : colors) {
-        
+    for (size_t i = 0; i < n_colors; i++) {
+        const Color& color = colors[i];
+
+        f << "|   " << setw(3) << i << "   |   " << setw(3) << color.red ;
     }
 }
 
 void print_color_row(ostream f, const Color &color) {
-      << "|   " << setw(3) << color << "   |   " << setw(3) << ;
 
 }
 
