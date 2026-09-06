3bf1eda19e67e9362247c6ea0e2a7d1a095b6433
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-05 22:40:44 -0700

    Automatic commit 090526224044

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # COMSC 210 Labs - Edwin B
-
-Lab 3 is runnable with run.sh or by compiling and running it
--- lab-3/main.cpp
+++ lab-3/main.cpp
@@ -12,6 +12,7 @@
 #include <iomanip>
 #include <fstream>
 #include <filesystem>
+#include <cstdlib>
 
 using namespace std;
 
