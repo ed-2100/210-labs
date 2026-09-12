664f9b8992578a447cb221b8238ce39380d8cba5
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-12 14:58:22 -0700

    Automatic commit 091226145822

--- lab-5/main.cpp
+++ lab-5/main.cpp
@@ -1,21 +1,32 @@
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+// TODO: add function declarations
+
 int main() {
     size_t n_arr = 5;
-    double* arr = static_cast<double*>(malloc(sizeof(double) * n_arr));
+    vector<double> arr;
+    arr.resize(n_arr);
+
+    if (!enterArrayData(arr.data(), arr.size())) {
+        cerr << "Failed to properly parse input!";
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
 
-bool enterArrayData(istream& in, double* arr, size_t n_arr) {
+bool enterArrayData(double* arr, size_t n_arr) {
+    cout << "Data entry for the array:\n";
 
+    for 
 }
 
-void outputArrayData(ostream& out, double* arr, size_t n_arr) {
+void outputArrayData(double* arr, size_t n_arr) {
 
 }
 
