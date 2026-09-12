be7e56e3b9670428dbab0851c8004b8117dfa416
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-12 14:48:22 -0700

    Automatic commit 091226144822

---
+++ lab-5/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    size_t n_arr = 5;
+    double* arr = static_cast<double*>(malloc(sizeof(double) * n_arr));
+
+    return EXIT_SUCCESS;
+}
+
+bool enterArrayData(istream& in, double* arr, size_t n_arr) {
+
+}
+
+void outputArrayData(ostream& out, double* arr, size_t n_arr) {
+
+}
+
+double sumArray() {
+
+}
