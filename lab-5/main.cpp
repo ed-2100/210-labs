d997355d19dbf166fbbc609cd9d143de81fc2027
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-12 15:28:22 -0700

    Automatic commit 091226152822

--- lab-5/main.cpp
+++ lab-5/main.cpp
@@ -7,21 +7,27 @@
 using namespace std;
 
 // TODO: add function declarations
+bool enterArrayData(double* arr, size_t n_arr);
+void outputArrayData(double* arr, size_t n_arr);
+double sumArray(double* arr, size_t n_arr);
 
 int main() {
-    size_t n_arr = 5;
     vector<double> arr;
-    arr.resize(n_arr);
+    arr.resize(5);
 
     if (!enterArrayData(arr.data(), arr.size())) {
         cerr << "Failed to properly parse input!";
         return EXIT_FAILURE;
     }
 
+    outputArrayData(arr.data(), arr.size());
+
+    cout << "Sum of values: " << sumArray(arr.data(), arr.size()) << '\n';
+
     return EXIT_SUCCESS;
 }
 
-bool enterArrayData(double* arr, size_t n_arr, bool retry_on_fail) {
+bool enterArrayData(double* arr, size_t n_arr) {
     cout << "Data entry for the array:\n";
 
     for (size_t i = 0; i < n_arr; i++) {
@@ -29,19 +35,31 @@ bool enterArrayData(double* arr, size_t n_arr, bool retry_on_fail) {
         
         if (!(cin >> arr[i])) {
             cerr << "Invalid number!";
-            break;
+            return false;
         }
     }
 
-    cout << "Data entry complete.";
-}
+    cout << "Data entry complete.\n";
 
+    return true;
+}
 
 void outputArrayData(double* arr, size_t n_arr) {
     cout << "Outputting array elements: ";
-    for (size_t i = 0)
+    
+    for (size_t i = 0; i < n_arr; i++) {
+        cout << arr[i] << ' ';
+    }
+
+    cout << '\n';
 }
 
-double sumArray() {
+double sumArray(double* arr, size_t n_arr) {
+    double sum = 0;
 
+    for (size_t i = 0; i < n_arr; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
 }
