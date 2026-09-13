205f3977444448765afb549c1be6276a015ac8fd
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-13 10:02:15 -0700

    Automatic commit 091326100215

--- lab-7/main.cpp
+++ lab-7/main.cpp
@@ -1,11 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
 int main() {
     
 }
 
-void reverseArray() {
-
+void reverseArray(string* arr, size_t n_arr) {
+    for (size_t a = 0, b = n_arr - 1; a < b; a++, b--) {
+        string temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 }
 
-void displayArray() {
-    
+void displayArray(string* arr, size_t n_arr) {
+
 }
