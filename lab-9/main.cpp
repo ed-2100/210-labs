7a75489afff42b37a4f3d05760342f5a13f29f1d
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-18 14:43:39 -0700

    Automatic commit 091826144339

--- lab-9/main.cpp
+++ lab-9/main.cpp
@@ -4,6 +4,7 @@
 
 #include <array>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,11 +18,11 @@ int main() {
     array<float, DIM> b;
     array<array<float, DIM + 1>, DIM> aug = {
         array<float, DIM + 1>
-        { 1,  2,  3,  4,  5,  6},
-        { 3,  4,  5,  6,  7,  8},
-        { 5,  6,  7,  8,  9, 10},
-        { 7,  8,  9, 10, 11, 12},
-        {11, 12, 13, 14, 15, 16}
+        {-59, -74,  69, -39, -62,  22},
+        {  9,  25, -72,   8, -32,  59},
+        {-72,  20, -50,  58,  62,  29},
+        { 95,   9, -90, -12,  95, -28},
+        {-51, -76, -31,  50,  -2,  -7},
     };
 
     print_array(aug);
@@ -34,9 +35,6 @@ int main() {
 
 void rref(array<array<float, DIM + 1>, DIM>& aug) {
     for (size_t i = 0, j = 0; j < DIM; i++, j++) {
-        print_array(aug);
-        cout << "\n\n";
-
         while (aug[i][j] == 0) {
             size_t r = DIM - 1;
 
@@ -65,7 +63,6 @@ void rref(array<array<float, DIM + 1>, DIM>& aug) {
             aug[j][c] *= rcp;
         }
 
-
         for (size_t r = i + 1; r < DIM; r++) {
             float k = aug[r][i];
 
@@ -82,10 +79,10 @@ void print_array(const array<array<float, DIM + 1>, DIM>& aug) {
             continue;
         }
 
-        cout << row[0];
+        cout << setprecision(3) << setw(7) << row[0];
 
         for (size_t i = 1; i < row.size(); i++) {
-            cout << ' ' << row[i];
+            cout << setprecision(3) << setw(7) << row[i];
         }
 
         cout << '\n';
