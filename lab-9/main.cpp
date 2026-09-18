62583701477d3d6e00c7f21dcd920b00add67202
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-18 15:43:39 -0700

    Automatic commit 091826154339

--- lab-9/main.cpp
+++ lab-9/main.cpp
@@ -9,21 +9,19 @@
 
 using namespace std;
 
-constexpr size_t DIM = 5;
+constexpr size_t NROW = 5;
+constexpr size_t NCOL = 6;
 
-void rref(array<array<float, DIM + 1>, DIM>& aug);
-void print_array(const array<array<float, DIM + 1>, DIM>& aug);
+void rref(array<float, NROW * NCOL>& aug);
+void print_array(const array<float, NROW * NCOL>& aug);
 
 int main() {
-    array<array<float, DIM>, DIM> A;
-    array<float, DIM> b;
-    array<array<float, DIM + 1>, DIM> aug = {
-        array<float, DIM + 1>
-        {-9, -4,  9, -9, -2,  2},
-        { 0,  0, -2,  0, -2,  9},
-        { 0,  3,  5,  0,  0,  9},
-        { 0,  0,  0, -2,  0, -8},
-        { 0,  0,  1,  0, -2, -7},
+    array<float, NROW * NCOL> aug = {
+        -9, -4,  9, -9, -2,  2,
+         0,  0, -2,  0, -2,  9,
+         0,  3,  5,  0,  0,  9,
+         0,  0,  0, -2,  0, -8,
+         0,  0,  1,  0, -2, -7,
     };
 
     print_array(aug);
@@ -33,66 +31,62 @@ int main() {
     print_array(aug);
 }
 
-void rref(array<array<float, DIM + 1>, DIM>& aug) {
-    for (size_t i = 0, j = 0; i < DIM && j < DIM + 1; i++, j++) {
-        while (aug[i][j] == 0) {
-            size_t r = DIM - 1;
+void rref(array<float, NROW * NCOL>& aug) {
+    for (size_t i = 0, j = 0; i < NROW && j < NCOL; i++, j++) {
+        while (aug[i * NCOL + j] == 0) {
+            size_t r = NROW - 1;
 
-            while (aug[r][j] == 0 && r > i) {
+            while (aug[r * NCOL + j] == 0 && r > i) {
                 r -= 1;
             }
 
             if (r == i) {
                 j += 1;
 
-                if (j >= DIM + 1) {
+                if (j >= NCOL) {
                     return;
                 }
 
                 continue;
             }
 
-            for (size_t c = i; c < DIM + 1; c++) {
-                swap(aug[i][c], aug[r][c]);
+            for (size_t c = i; c < NCOL; c++) {
+                swap(aug[i * NCOL + c], aug[r * NCOL + c]);
             }
         }
 
-        float rcp = 1.0 / aug[i][j];
+        float rcp = 1.0 / aug[i * NCOL + j];
 
-        aug[i][j] = 1;
-        for (size_t c = j + 1; c < DIM + 1; c++) {
-            aug[i][c] *= rcp;
+        aug[i * NCOL + j] = 1;
+        for (size_t c = j + 1; c < NCOL; c++) {
+            aug[i * NCOL + c] *= rcp;
         }
 
-        for (size_t r = 0; r < DIM; r++) {
+        for (size_t r = 0; r < NROW; r++) {
             if (r == i) {
                 continue;
             }
 
-            float k = aug[r][j];
+            float k = aug[r * NCOL + j];
 
             if (k == 0) {
                 continue;
             }
 
-            aug[r][j] = 0;
-            for (size_t c = j + 1; c < DIM + 1; c++) {
-                aug[r][c] -= aug[i][c] * k;
+            aug[r * NCOL + j] = 0;
+            for (size_t c = j + 1; c < NCOL; c++) {
+                aug[r * NCOL + c] -= aug[i * NCOL + c] * k;
             }
         }
     }
 }
 
-void print_array(const array<array<float, DIM + 1>, DIM>& aug) {
-    for (const auto& row : aug) {
-        if (row.size() == 0) {
-            continue;
-        }
+void print_array(const array<float, NROW * NCOL>& aug) {
+    for (size_t r = 0; r < NROW; r++) {
+        cout << ' ' << aug[r * NCOL];
 
-        cout << ' ' << row[0];
-
-        for (size_t i = 1; i < row.size(); i++) {
-            cout << ' ' << row[i];
+        for (size_t c = 1; c < NCOL; c++) {
+            cout << ' ' << aug[r * NCOL + c];
         }
 
         cout << '\n';
