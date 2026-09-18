61b4c379bb97829965459c5addd7d6e121a5cdb3
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-18 13:13:39 -0700

    Automatic commit 091826131339

---
+++ lab-9/main.cpp
@@ -0,0 +1,14 @@
+// System of linear equations solver, limited to 5x5 A matrices.
+
+// Given Ax = b, solve for x.
+
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+array<float, 
+
+int main() {
+
+}
