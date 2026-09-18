73e0edc9a43fc4fe48a795bba248d9dedac16960
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-18 14:13:39 -0700

    Automatic commit 091826141339

--- lab-9/main.cpp
+++ lab-9/main.cpp
@@ -9,6 +9,9 @@ using namespace std;
 
 constexpr size_t DIM = 5;
 
+void rref(array<array<float, DIM + 1>, DIM>& aug);
+void print_array(const array<array<float, DIM + 1>, DIM>& aug);
+
 int main() {
     array<array<float, DIM>, DIM> A;
     array<float, DIM> b;
@@ -20,23 +23,24 @@ int main() {
         { 7,  8,  9, 10, 11, 12},
         {11, 12, 13, 14, 15, 16}
     };
+
+    print_array(aug);
+
+    cout << "\n\n";
+
+    rref(aug);
+    print_array(aug);
 }
 
 void rref(array<array<float, DIM + 1>, DIM>& aug) {
-    array<float, DIM + 1> row;
-
-    for (size_t i = 0; i < DIM - 1; i++) {
+    for (size_t i = 0; i < DIM; i++) {
         float rcp = 1.0 / aug[i][i];
 
         for (size_t c = i; c < DIM + 1; c++) {
             aug[i][c] *= rcp;
         }
 
-        for (size_t r = 0; r < DIM; r++) {
-            if (r == i) {
-                continue;
-            }
-
+        for (size_t r = i + 1; r < DIM; r++) {
             float k = aug[r][i];
 
             for (size_t c = i; c < DIM + 1; c++) {
@@ -48,6 +52,16 @@ void rref(array<array<float, DIM + 1>, DIM>& aug) {
 
 void print_array(const array<array<float, DIM + 1>, DIM>& aug) {
     for (const auto& row : aug) {
-        for (const auto& item : aug)
+        if (row.size() == 0) {
+            continue;
+        }
+
+        cout << row[0];
+
+        for (size_t i = 1; i < row.size(); i++) {
+            cout << ' ' << row[i];
+        }
+
+        cout << '\n';
     }
 }
