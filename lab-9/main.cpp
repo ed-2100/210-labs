094814d5fd9c4cf83c9261f10b51dd637285d658
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-18 13:53:39 -0700

    Automatic commit 091826135339

--- lab-9/main.cpp
+++ lab-9/main.cpp
@@ -26,16 +26,19 @@ void rref(array<array<float, DIM + 1>, DIM>& aug) {
     array<float, DIM + 1> row;
 
     for (size_t i = 0; i < DIM - 1; i++) {
-        float n1 = aug[i][i];
-        float n2 = aug[i][i];
+        float rcp = 1.0 / aug[i][i];
 
-        float q = - n2 / n1;
-
-        for (size_t j = i; j < DIM + 1; j++) {
-            row[j] = aug[i][j] * q;
+        for (size_t c = i; c < DIM + 1; c++) {
+            aug[i][c] *= rcp;
         }
 
-        for (size_t r = 0; r < )
+        for (size_t r = 0; r < i; r++) {
+
+        }
+
+        for (size_t r = i + 1; r < DIM; r++) {
+
+        }
     }
 }
 
