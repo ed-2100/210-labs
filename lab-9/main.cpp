bc9a8e157dcb61955ebb8ba52f80a1b777c8954d
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-18 13:23:39 -0700

    Automatic commit 091826132339

--- lab-9/main.cpp
+++ lab-9/main.cpp
@@ -7,8 +7,21 @@
 
 using namespace std;
 
-array<float, 
+constexpr int DIM = 5;
 
 int main() {
+    array<array<float, DIM>, DIM> A;
+    array<float, DIM> b;
+    array<array<float, DIM + 1>, DIM> aug = {
+        array<float, DIM + 1>
+        { 1,  2,  3,  4,  5,  6},
+        { 3,  4,  5,  6,  7,  8},
+        { 5,  6,  7,  8,  9, 10},
+        { 7,  8,  9, 10, 11, 12},
+        {11, 12, 13, 14, 15, 16}
+    };
+}
+
+void rref(array<array<float, DIM + 1>, DIM>& aug) {
 
 }
