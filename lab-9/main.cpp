4e3a5735459dab8e3f87e00724825980bcc47092
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-18 20:11:20 -0700

    Lab 9, Milestone 1 - Duplicate the code for `std::vector`

--- lab-9/main.cpp
+++ lab-9/main.cpp
@@ -1,6 +1,9 @@
 // COMSC-210 | Lab 9 | Edwin Burwell
 //
-// Compute the reduced row-echelon form of a 5x6 matrix.
+// Note: This code may or may not use C++23.
+//
+// Computes the reduced row-echelon form of a 5x6 matrix
+// using `std::array` and `std::vector`.
 
 #include <array>
 #include <iostream>
@@ -8,8 +11,13 @@
 #include <cmath>
 #include <filesystem>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+
+// ======================
+// ===== std::array =====
+// ======================
 
 // The row reducer implementation.
 //
@@ -19,7 +27,7 @@ using namespace std;
 //
 // - `aug`: The matrix to be reduced, stored in row-major order.
 template <size_t NROW, size_t NCOL>
-void rref(array<float, NROW * NCOL>& mat);
+void rref_a(array<float, NROW * NCOL>& mat);
 
 // The matrix display function.
 //
@@ -29,7 +37,7 @@ void rref(array<float, NROW * NCOL>& mat);
 //
 // - `aug`: The matrix to display, stored in row-major order.
 template <size_t NROW, size_t NCOL>
-void print_array(const array<float, NROW * NCOL>& mat);
+void print_array_a(const array<float, NROW * NCOL>& mat);
 
 // The matrix loader.
 //
@@ -39,7 +47,38 @@ void print_array(const array<float, NROW * NCOL>& mat);
 // 
 // - `aug`: The array to load the matrix into.
 template <size_t NROW, size_t NCOL>
-void load_array(array<float, NROW * NCOL>& mat, const char* path);
+void load_array_a(array<float, NROW * NCOL>& mat, const char* path);
+
+// =======================
+// ===== std::vector =====
+// =======================
+
+// The row reducer implementation.
+//
+// Converts `aug` to reduced row-echelon form.
+//
+// ### Arguments
+//
+// - `aug`: The matrix to be reduced, stored in row-major order.
+void rref_v(vector<float>& mat, size_t n_row, size_t n_col);
+
+// The matrix display function.
+//
+// Prints `aug` to `stdout`.
+//
+// ### Arguments
+//
+// - `aug`: The matrix to display, stored in row-major order.
+void print_array_v(const vector<float>& mat, size_t n_row, size_t n_col);
+
+// The matrix loader.
+//
+// Loads a matrix from the file specified by `path` into row-major order.
+//
+// ### Arguments
+// 
+// - `aug`: The array to load the matrix into.
+void load_array_v(vector<float>& mat, size_t n_row, size_t n_col, const char* path);
 
 // The main function.
 //
@@ -58,27 +97,56 @@ int main(int argc, const char** argv) {
     constexpr size_t NROW = 5;
     constexpr size_t NCOL = 6;
 
-    array<float, NROW * NCOL> mat;
+    // ======================
+    // ===== std::array =====
+    // ======================
+
+    array<float, NROW * NCOL> mat_a;
 
     auto file = filesystem::path(argv[0]).parent_path() / "test_sample.txt";
 
-    load_array<NROW, NCOL>(mat, file.c_str());
+    load_array_a<NROW, NCOL>(mat_a, file.c_str());
 
-    cout << "Input array:\n";
+    cout << "Input array (std::array):\n";
 
-    print_array<NROW, NCOL>(mat);
+    print_array_a<NROW, NCOL>(mat_a);
 
-    rref<NROW, NCOL>(mat);
+    rref_a<NROW, NCOL>(mat_a);
 
-    cout << "\n\nReduced Row-Echelon Form:\n";
+    cout << "\n\nReduced Row-Echelon Form (std::array):\n";
 
-    print_array<NROW, NCOL>(mat);
+    print_array_a<NROW, NCOL>(mat_a);
+
+    cout << "\n\n";
+
+    // =======================
+    // ===== std::vector =====
+    // =======================
+
+    vector<float> mat_v;
+    mat_v.resize(NROW * NCOL);
+
+    load_array_v(mat_v, NROW, NCOL, file.c_str());
+
+    cout << "Input array (std::vector):\n";
+
+    print_array_v(mat_v, NROW, NCOL);
+
+    rref_v(mat_v, NROW, NCOL);
+
+    cout << "\n\nReduced Row-Echelon Form (std::vector):\n";
+
+    print_array_v(mat_v, NROW, NCOL);
 
     cout << '\n';
 }
 
+// ======================
+// ===== std::array =====
+// ======================
+
 template <size_t NROW, size_t NCOL>
-void rref(array<float, NROW * NCOL>& mat) {
+void rref_a(array<float, NROW * NCOL>& mat) {
     for (size_t i = 0, j = 0; i < NROW && j < NCOL; i++, j++) {
         while (mat.at(i * NCOL + j) == 0) {
             size_t r = NROW - 1;
@@ -129,7 +197,7 @@ void rref(array<float, NROW * NCOL>& mat) {
 }
 
 template <size_t NROW, size_t NCOL>
-void print_array(const array<float, NROW * NCOL>& mat) {
+void print_array_a(const array<float, NROW * NCOL>& mat) {
     if (mat.size() == 0) {
         return;
     }
@@ -154,7 +222,102 @@ void print_array(const array<float, NROW * NCOL>& mat) {
 }
 
 template <size_t NROW, size_t NCOL>
-void load_array(array<float, NROW * NCOL>& mat, const char* path) {
+void load_array_a(array<float, NROW * NCOL>& mat, const char* path) {
+    ifstream file(path);
+
+    size_t i = 0;
+
+    string line;
+    while (getline(file, line, '\n')) {
+        stringstream line_stream(line);
+        string item;
+        while (getline(line_stream, item, ',')) {
+            mat[i] = stof(item);
+
+            i += 1;
+        }
+    }
+}
+
+// =======================
+// ===== std::vector =====
+// =======================
+
+void rref_v(vector<float>& mat, size_t n_row, size_t n_col) {
+    for (size_t i = 0, j = 0; i < n_row && j < n_col; i++, j++) {
+        while (mat.at(i * n_col + j) == 0) {
+            size_t r = n_row - 1;
+
+            while (mat.at(r * n_col + j) == 0 && r > i) {
+                r -= 1;
+            }
+
+            if (r == i) {
+                j += 1;
+
+                if (j >= n_col) {
+                    return;
+                }
+
+                continue;
+            }
+
+            for (size_t c = i; c < n_col; c++) {
+                swap(mat.at(i * n_col + c), mat.at(r * n_col + c));
+            }
+        }
+
+        float rcp = 1.0 / mat.at(i * n_col + j);
+
+        mat.at(i * n_col + j) = 1;
+        for (size_t c = j + 1; c < n_col; c++) {
+            mat.at(i * n_col + c) *= rcp;
+        }
+
+        for (size_t r = 0; r < n_row; r++) {
+            if (r == i) {
+                continue;
+            }
+
+            float k = mat.at(r * n_col + j);
+
+            if (k == 0) {
+                continue;
+            }
+
+            mat.at(r * n_col + j) = 0;
+            for (size_t c = j + 1; c < n_col; c++) {
+                mat.at(r * n_col + c) -= mat.at(i * n_col + c) * k;
+            }
+        }
+    }
+}
+
+void print_array_v(const vector<float>& mat, size_t n_row, size_t n_col) {
+    if (mat.size() == 0) {
+        return;
+    }
+
+    auto print_line = [&mat, n_col](size_t r) {
+        auto print_item = [&mat, n_col, &r](size_t c) {
+            cout << mat.at(r * n_col + c);
+        };
+
+        print_item(0);
+        for (size_t c = 1; c < n_col; c++) {
+            cout << ' ';
+            print_item(c);
+        }
+    };
+
+    print_line(0);
+    for (size_t r = 1; r < n_row; r++) {
+        cout << '\n';
+        print_line(r);
+    }
+}
+
+void load_array_v(vector<float>& mat, size_t n_row, size_t n_col, const char* path) {
     ifstream file(path);
 
     size_t i = 0;
