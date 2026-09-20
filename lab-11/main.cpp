ef8fa95ff4a893d5bb508893e2f0df3c61f59dd1
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-20 14:43:26 -0700

    Automatic commit 092026144326

--- lab-11/main.cpp
+++ lab-11/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <algorithm>
 #include <optional>
+#include <utility>
 
 using namespace std;
 
@@ -16,36 +17,66 @@ struct KVPair {
 };
 
 struct HashMap {
-    vector<vector<KVPair>> storage;
+    vector<vector<KVPair>> coarse;
     size_t count;
 
     optional<string> put(string key, string value);
-    string& get(string key);
+    optional<string*> get(string key);
     string remove(string key);
     void resize(size_t n);
 };
 
 optional<string> HashMap::put(string key, string value) {
-    if (count >= storage.size()) {
-        resize(storage.size() << 1);
+    if (count >= coarse.size()) {
+        resize(coarse.size() << 1);
     }
 
     size_t key_hash = hash<string>{}(key);
 
-    size_t idx_broad = key_hash % storage.size();
+    size_t idx_broad = key_hash % coarse.size();
+
+    vector<KVPair>& fine = coarse[idx_broad];
 
     auto it_narrow = find(
-        storage[idx_broad].begin(),
-        storage[idx_broad].end(),
+        fine.begin(),
+        fine.end(),
         [&key](auto&& k){
             return key == k;
         }
     );
 
-    if (it_narrow == storage[idx_broad].end()) { // Doesn't exist.
-        
-    } else { // Already exists.
+    if (it_narrow == fine.end()) { // Doesn't exist.
+        fine.push_back(KVPair {
+            .key = move(key),
+            .value = move(value)
+        });
 
+        return {};
+    } else { // Already exists.
+        return exchange(it_narrow.base()->value, value);
+    }
+}
+
+optional<string*> HashMap::get(string key) {
+    size_t key_hash = hash<string>{}(key);
+
+    size_t idx_broad = key_hash % coarse.size();
+
+    vector<KVPair>& fine = coarse[idx_broad];
+
+    auto it_narrow = find(
+        fine.begin(),
+        fine.end(),
+        [&key](auto&& k){
+            return key == k;
+        }
+    );
+
+
+    if (it_narrow == fine.end()) { // Doesn't exist.
+        return {};
+    } else { // Already exists.
+        return exchange(it_narrow.base()->value, value);
     }
 }
 
