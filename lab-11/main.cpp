76af2a051656f9a72471b5eb697705e9f37ddfb3
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-20 15:53:26 -0700

    Automatic commit 092026155326

--- lab-11/main.cpp
+++ lab-11/main.cpp
@@ -27,8 +27,8 @@ struct HashMap {
 
 optional<string> hashmap_put(HashMap& map, string key, string value);
 string* hashmap_get(HashMap& map, string key);
-optional<string> hashmap_remove(HashMap map, string key);
-void hashmap_resize(HashMap map, size_t n);
+optional<string> hashmap_remove(HashMap& map, string key);
+void hashmap_resize(HashMap& map, size_t n);
 
 int main() {
     HashMap map;
@@ -46,7 +46,7 @@ int main() {
     hashmap_put(map, "Shirt Color", "Blue");
 
     cout << " Done.\nGetting \"Name\"...";
-    
+
     string* maybe_name = hashmap_get(map, "Name");
     cout << "\nName: "
          << (maybe_name ? *maybe_name : not_found)
@@ -55,7 +55,8 @@ int main() {
 
 optional<string> hashmap_put(HashMap& map, string key, string value) {
     if (map.count >= map.coarse.size()) {
-        hashmap_resize(map, map.coarse.size() == 0 ? 1 : map.coarse.size() );
+        size_t n = map.coarse.size() == 0 ? 1 : map.coarse.size();
+        hashmap_resize(map, n);
     }
 
     size_t key_hash = hash<string>{}(key);
@@ -107,7 +108,7 @@ string* hashmap_get(HashMap& map, string key) {
     }
 }
 
-optional<string> hashmap_remove(HashMap map, string key) {
+optional<string> hashmap_remove(HashMap& map, string key) {
     size_t key_hash = hash<string>{}(key);
 
     size_t idx_broad = key_hash % map.coarse.size();
@@ -135,7 +136,7 @@ optional<string> hashmap_remove(HashMap map, string key) {
     }
 }
 
-void hashmap_resize(HashMap map, size_t n) {
+void hashmap_resize(HashMap& map, size_t n) {
     if (n <= map.coarse.size()) {
         return;
     }
@@ -150,5 +151,5 @@ void hashmap_resize(HashMap map, size_t n) {
         }
     }
 
-    map.coarse = coarse_new;
+    map.coarse = move(coarse_new);
 }
