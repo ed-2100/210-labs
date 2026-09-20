7a87091f221430e962dc7c9f6b2543d5d6f295dc
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-20 14:33:26 -0700

    Automatic commit 092026143326

--- lab-11/main.cpp
+++ lab-11/main.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <algorithm>
+#include <optional>
 
 using namespace std;
 
@@ -17,20 +19,34 @@ struct HashMap {
     vector<vector<KVPair>> storage;
     size_t count;
 
-    void put(string key, string value);
+    optional<string> put(string key, string value);
     string& get(string key);
     string remove(string key);
     void resize(size_t n);
 };
 
-void HashMap::put(string key, string value) {
+optional<string> HashMap::put(string key, string value) {
     if (count >= storage.size()) {
         resize(storage.size() << 1);
     }
 
-    auto key_hash = hash<string>{}(key);
+    size_t key_hash = hash<string>{}(key);
 
-    
+    size_t idx_broad = key_hash % storage.size();
+
+    auto it_narrow = find(
+        storage[idx_broad].begin(),
+        storage[idx_broad].end(),
+        [&key](auto&& k){
+            return key == k;
+        }
+    );
+
+    if (it_narrow == storage[idx_broad].end()) { // Doesn't exist.
+        
+    } else { // Already exists.
+
+    }
 }
 
 int main() {
