f9de9ca993f83ba9bcca48448d7e513e8c70a7bf
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-25 14:29:16 -0700

    Automatic commit 092526142916

--- lab-13/main.cpp
+++ lab-13/main.cpp
@@ -9,16 +9,30 @@ struct Student {
     float grade;
 };
 
+template <typename T>
+void sort(T* arr, size_t n_arr);
+
 int main(int argc, const char** argv) {
     filesystem::path exe_path(argv[0]);
 
     auto exe_dir = exe_path.parent_path();
 
     auto grades_path = exe_dir / "210-lab-13-grades.txt";
-
-    ifstream grades_file(grades_path);
+    auto sorted_grades_path = exe_dir / "210-lab-13-grades-sorted.txt";
 
     vector<Student> students;
+
+    read_grades(students, grades_path);
+
+    sort(students.data(), students.size());
+
+    write_grades(students, sorted_grades_path);
+}
+
+void read_grades(vector<Student> &students, filesystem::path file) {
+    students.clear();
+
+    ifstream grades_file(file);
 
     string buf;
     string segment;
@@ -39,16 +53,15 @@ int main(int argc, const char** argv) {
             .grade = grade,
         });
     }
-
 }
 
 template <typename T>
 void sort(T* arr, size_t n_arr) {
-    for (size_t i = 0; i < n_arr; i++) {
+    for (size_t i = 0; i < n_arr - 1; i++) {
         T* current = &arr[i]
-        T* max = cur;
+        T* max = current;
 
-        for (size_t j = i + 1; j++) {
+        for (size_t j = i + 1; j < n_arr; j++) {
             T& tmp = arr[j];
 
             if (tmp > *max) {
@@ -56,8 +69,14 @@ void sort(T* arr, size_t n_arr) {
             }
         }
 
-        if (max != current) {
+        if (current != max) {
             swap(*current, *max);
         }
     }
+}
+
+void write_grades(const vector<Student> &students, filesystem::path file) {
+    ofstream grades_file;
+
+    grades_file.
 }
