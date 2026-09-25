2837e0e4a43491118e3d53d4f58fec2354d2afb8
Edwin Burwell <edwinburwell758@gmail.com>
2026-09-25 13:49:16 -0700

    Automatic commit 092526134916

--- lab-13/main.cpp
+++ lab-13/main.cpp
@@ -1,3 +1,19 @@
-int main(int argc, const char** argv) {
+#include <filesystem>
+#include <fstream>
 
+using namespace std;
+
+struct Student {
+    uint32_t id;
+    float grade;
+};
+
+int main(int argc, const char** argv) {
+    filesystem::path exe_path(argv[0]);
+
+    auto exe_dir = exe_path.parent_path();
+
+    auto grades_path = exe_dir / "210-lab-13-grades.txt";
+
+    ifstream grades_file();
 }
